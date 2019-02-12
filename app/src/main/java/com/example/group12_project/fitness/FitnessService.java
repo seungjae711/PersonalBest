59abc84c61ec34dbb19842672c14b14afa6f5e29
DerekWang <hardtogetone@gmail.com>
2019-02-11 19:34:24 -0800

    recording

--- app/src/main/java/com/example/group12_project/fitness/FitnessService.java
+++ app/src/main/java/com/example/group12_project/fitness/FitnessService.java
@@ -1,4 +1,6 @@
 package com.example.group12_project.fitness;
 
 public interface FitnessService {
+    int getRequestCode();
+    void setup();
 }
--- app/src/main/java/com/example/group12_project/fitness/GoogleFitAdapter.java
+++ app/src/main/java/com/example/group12_project/fitness/GoogleFitAdapter.java
@@ -1,8 +1,20 @@
 package com.example.group12_project.fitness;
 
-import com.example.group12_project.MainActivity;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
-public class GoogleFitAdapter {
+import com.example.group12_project.MainActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class GoogleFitAdapter implements FitnessService {
 
     // for fit api permission d
     private final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE
@@ -18,4 +30,65 @@ public class GoogleFitAdapter {
     public GoogleFitAdapter(MainActivity activity) {
         this.activity = activity;
     }
+
+    // we need this for google fit
+    @Override
+    public int getRequestCode() {
+        return GOOGLE_FIT_PERMISSIONS_REQUEST_CODE;
+    }
+
+    // setup the fitness tracking options
+    public void setup() {
+
+        // declaring the Fit API data types and access required
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+
+        // check if the user has previously granted the necessary data access
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(activity),
+                fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    activity,
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(activity),
+                    fitnessOptions
+            );
+        } else {
+            startRecording();
+        }
+    }
+
+    // subscribe step count for Google to record
+    public void startRecording() {
+
+        GoogleSignInAccount lastSignedInAccount = GoogleSignIn.getLastSignedInAccount(activity);
+
+        // check if already signed in
+        if (lastSignedInAccount == null) {
+            return;
+        }
+
+        // subscribe
+        Fitness.getHistoryClient(activity, lastSignedInAccount)
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener(new OnSuccessListener<DataSet>() {
+                    @Override
+                    public void onSuccess(DataSet dataSet) {
+                        Log.d(TAG, dataSet.toString());
+                        long total = dataSet.isEmpty() ? 0 :
+                                dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                        Log.d(TAG, "Total steps: " + total);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "There was a problem getting the step count.", e);
+                    }
+                });
+    }
+
+
 }

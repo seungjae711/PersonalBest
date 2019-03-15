5ed24020c5ff9d4a52450f0babeacf7b71bdf6ed
mayamiller <msm016@ucsd.edu>
2019-03-15 15:28:39 -0700

    Test Step Session

--- app/src/androidTest/java/com/example/group12_project/StepSessionTest.java
+++ app/src/androidTest/java/com/example/group12_project/StepSessionTest.java
@@ -1,4 +1,48 @@
 package com.example.group12_project;
 
-public class StepSessionTest {
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class StepSessionTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity activity;
+    public StepSessionTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        activity = getActivity();
+    }
+
+    @Test
+    public void testConstructor() {
+        stepSession session = new stepSession( activity);
+        assertNotNull( session);
+    }
+
+    @Test
+    public void testCalculate() {
+        stepSession session = new stepSession( activity);
+        session.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        session.end();
+        assertTrue(session.calculateSessionSpeed() > 1000);
+    }
+
+    @Test
+    public void testSteps() {
+        stepSession session = new stepSession( activity);
+        assertTrue( session.getSessionSteps() >= 0);
+    }
+
 }

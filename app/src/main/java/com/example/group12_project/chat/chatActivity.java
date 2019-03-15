b72724806f87b8b9f76e48c7f5fa6820ebd4d11f
menethil <zyjeagle@outlook.com>
2019-03-15 10:30:10 -0700

    fixed cloud goal update

--- app/src/main/java/com/example/group12_project/chat/chatActivity.java
+++ app/src/main/java/com/example/group12_project/chat/chatActivity.java
@@ -7,9 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
